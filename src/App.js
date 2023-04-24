2828bc79b0a7ac6b2119a65e326199ce6ba50a67
Felix Rodriguez <108102495+ItsFelixRodriguez@users.noreply.github.com>
2023-04-24 11:44:39 +0900

    Making sure AppComponent is importing correctly

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import AppComponent from './components/App/AppComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <body>
-        <p>Hey. This is Felix. The Developer of this playlist app. </p>
-      </body>
-    </div>
+    <AppComponent />
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    //   <body>
+
+    //     <p>Hey. This is Felix. The Developer of this playlist app. </p>
+    //   </body>
+    // </div>
   );
 }
 
