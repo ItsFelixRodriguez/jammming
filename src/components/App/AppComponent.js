e5e36eea8886057c5be52485e52ec0c7fe3bf221
Felix Rodriguez <108102495+ItsFelixRodriguez@users.noreply.github.com>
2023-04-24 12:00:42 +0900

    Messing around with importing css into a component

--- src/components/App/AppComponent.js
+++ src/components/App/AppComponent.js
@@ -1,8 +1,16 @@
 import React from "react";
+import './App.css'
+
 
 function AppComponent() {
     return (
+        <div>
         <h1>Test Test</h1>
+        <p> Here's some math! 1 + 1 = {1+1} </p>
+        <p>^ I coded that math problem into the react file. React is pretty cool and honestly refreshingly fast so far. 
+        </p>
+        </div>
+
     )
 }
 
