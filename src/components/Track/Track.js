7b190d0fa08aa109cf6c48f0bfa2d63a497c03e9
Felix Rodriguez <108102495+ItsFelixRodriguez@users.noreply.github.com>
2023-04-26 11:14:57 +0900

    Testing out a button counter

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import './Track.css';
 
 function Track() {
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        setCount(count +1);
+    }
+
     return (
         <div>
          <p>I'm a track component exported to a tracklist component</p>
-        </div>
+      
+            <button onClick={handleClick}>
+                I have been clicked {count} times
+            </button>
+            </div>
     )
 }
 
