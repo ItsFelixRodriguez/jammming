62561b392882bf186d202094d00122de53755e45
Felix Rodriguez <108102495+ItsFelixRodriguez@users.noreply.github.com>
2023-05-02 17:20:13 +0900

    finally added a search bar

--- src/components/App/AppComponent.js
+++ src/components/App/AppComponent.js
@@ -1,14 +1,18 @@
 import React from "react";
 import './App.css'
 import Playlist from "../Playlist/Playlist";
-import Tracklist from "../Tracklist/Tracklist";
+// import Tracklist from "../Tracklist/Tracklist";
 import SearchResults from "../SearchResults/SearchResults";
+import SearchBar   from "../SearchBar/SearchBar"
 
 
 function AppComponent() {
     return (
         <div>
         <h1>Jammming - Playlist Tool</h1>
+            <div className="searchBar">
+                <SearchBar /> 
+            </div>
              <div className="container">
                 <SearchResults />
                 <Playlist />
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,2 +1,21 @@
-import React from 'react';
-import './SearchBar.css';
+import React, { useState } from "react";
+import './SearchBar.css'
+
+    const SearchBar = () => {
+      const [value, setValue] = useState('');
+    
+      return (
+        <div>
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button onClick={() => {
+            console.log(value);
+          }}>Search</button>
+        </div>
+      );
+    };
+    
+    export default SearchBar;
